2bb421552d9b49bf789d0e353db96b86d39b617b
Victor Pluvinage <victorpluvinage25@gmail.com>
2025-07-21 22:46:58 +0200

    Try base64 coding

--- popUp.js
+++ popUp.js
@@ -1,4 +1,10 @@
 document.getElementById("submitButton").addEventListener("click", () => {
     const value = document.getElementById("userInput").value;
-    document.getElementById("output").textContent = value;
+    
+    // Hash the input value
+    const hashedValue = btoa(value);
+    // Send the hashed value to the background script
+
+    const output = document.getElementById("output");
+    output.textContent = `Your password: ${hashedValue}`;
 });
